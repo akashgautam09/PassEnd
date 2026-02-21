fcc62c12d089c13ae4d87a5250ad934fc65ec95a
Akash Gautam <panditaakashji111@gmail.com>
2026-02-21 18:59:15 +0530

    added handleChange, showPassord, handleSubmit

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,20 +1,49 @@
-import { useState } from "react";
+import { useState } from "react"
+
 const Manager = () => {
-    const [isTyping, setIsTyping] = useState(false);
+    const [showPass, setshowPass] = useState("bi-eye-fill")
+    const [form, setform] = useState({ site: "", username: "", password: "" })
+
+    const handleChange = (e) => {
+        setform({ ...form, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = () => {
+        console.log(form)
+    }
+
+    const showPassword = () => {
+        if (showPass === "bi-eye-fill") {
+            setshowPass("bi-eye-slash-fill")
+        }
+        else setshowPass("bi-eye-fill")
+    }
     return (
         <>
-            <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-size-[6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_100%_400px,#ffc0fa9c,transparent)]"></div></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-purple-50 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-size-[6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_100%_400px,#e4a3f599,transparent)]"></div></div>
             <div className="w-4xl mx-auto">
-                <div className="flex flex-col justify-center items-center py-6">
-                    <h1 className="flex font-semibold font-serif text-2xl"><span className="text-[#00908b]">&lt; Pass</span><span>End /&gt;</span></h1>
+                <div className="flex flex-col justify-center items-center py-4 my-5">
+                    <h1 className="flex font-semibold font-serif text-2xl"><span className="text-[#007773]">&lt; Pass</span><span>End /&gt;</span></h1>
                     <p className='font-sans font-semibold text-xl'>Your own password manager</p>
                 </div>
-                <div className="text-white">
-                    <div className="my-2"><input type="text" name="" id="" className="w-full bg-gray-400 py-2 px-2 rounded-3xl opacity-50" /></div>
-                    <div className="flex justify-between">
-                        <input type="text" name="" id="" className={`bg-gray-400 rounded-3xl py-2 px-2 opacity-50 w-2/5 ${isTyping ? "border-2 border-green-300" : "border-2 border-transparent"}`} onChange={() => { setIsTyping(true) }} />
-                        <input type="text" name="" id="" className="bg-gray-400 rounded-3xl py-2 px-2 opacity-50 w-1/3 " />
+                <div className="flex flex-col gap-6 items-center">
+                    <div className="my-2 w-full">
+                        <input type="text" name="site" id="" placeholder="Enter website URL" className="w-full font-sans font-semibold border-2 border-purple-700 py-2 px-2 rounded-3xl opacity-50" onChange={handleChange} />
                     </div>
+                    <div className="flex justify-between relative gap-3 w-full">
+                        <input type="text" name="username" id="" placeholder="Enter Username" className="text-black font-sans font-semibold border-2 border-purple-700 rounded-3xl py-2 px-2 opacity-50 w-2/3" onChange={handleChange} />
+                        <input type="password" name="password" id="" placeholder="Enter Password" required className="font-sans font-semibold border-2 border-purple-700 rounded-3xl py-2 px-2 opacity-50 w-1/3" onChange={handleChange} />
+                        <i className={`bi ${showPass}
+                        text-2xl absolute top-1 right-2 cursor-pointer`} onClick={showPassword} ></i>
+                    </div>
+                    <button type="submit" className="flex items-center cursor-pointer bg-[#c68bfe] hover:bg-purple-400 duration-150 w-fit rounded-3xl px-4 py-1.5 font-sans font-semibold text-2xs gap-0.5"
+                        onClick={handleSubmit}
+                    >Add Password
+                        <lord-icon
+                            src="https://cdn.lordicon.com/efxgwrkc.json"
+                            trigger="hover"
+                            style={{ width: "30px", height: "30px" }}>
+                        </lord-icon>
+                    </button>
                 </div>
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between bg-purple-300 px-7 py-5 shadow-lg/10">
-      <div className="flex font-semibold font-serif text-2xl"><span className="text-[#00908b]">&lt; Pass</span><span>End /&gt;</span></div>
+      <div className="flex font-semibold font-serif text-2xl"><span className="text-[#007773]">&lt; Pass</span><span>End /&gt;</span></div>
       <ul className="flex items-center gap-7 font-semibold font-serif">
         <li><a href="/">Home</a></li>
         <li><a href="#">Blog</a></li>
