bff72ffcc972e330bfa91cd53c1a8f80c9cb5b51
Akash Gautam <panditaakashji111@gmail.com>
2026-04-30 21:12:27 +0530

    added gtihub and linkedin URLs to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,14 @@ const Footer = () => {
             <div className='flex items-center font-semibold text-lg text-white'>Created with<span className='flex justify-center items-center px-2'>
                 <i className='bi bi-heart-fill text-red-500 animate-pulse drop-shadow-[0_0_5px_rgba(239,68,68,0.8)]'></i>
             </span>by Akash Gautam</div>
+            <div className='flex gap-4 mt-2'>
+                <a href="https://github.com/akashgautam09" target="_blank" rel="noopener noreferrer" className='text-white hover:text-[#00a09b] transition'>
+                    <i className='bi bi-github text-xl'></i>
+                </a>
+                <a href="https://www.linkedin.com/in/akash-gautam-07664230a/" target="_blank" rel="noopener noreferrer" className='text-white hover:text-[#00a09b] transition'>
+                    <i className='bi bi-linkedin text-xl'></i>
+                </a>
+            </div>
         </div>
     )
 }
